d436c8475d35579120c691ad1668f61f05eedb19
Constantin Zorile <czorile@gmail.com>
2021-03-08 22:08:41 +0200

    exercise 1-12

--- chapter_1/exercise_1_12.c
+++ chapter_1/exercise_1_12.c
@@ -1,16 +1,39 @@
 
 #include <stdio.h>
 
+#define INSIDE_THE_WORD   1
+#define OUTSIDE_THE_WORD  0
+
 int main(void) {
 
+  /**
+   * We're using Finite State Machine concept:
+   * INSIDE_THE_WORD: a state in which we print every input character
+   * OUTISDE_THE_WORD: a state we switch to when the input is 
+   *  either a space or a tab character (common separators between words).
+   *  In this case, we convert space or \t into a \n, but don't do
+   *  anything else.
+   * If, again, input is different from space or tab, we go back
+   * to the initial state and resume our printing.
+   * Not printing explicitly \t character is by design.
+   */
+
   int c;
+  int state;
+
+  state = OUTSIDE_THE_WORD;
 
   while ((c = getchar()) != EOF) {
-    putchar(c);
     if (c == ' ' || c == '\t') {
+      if (state == INSIDE_THE_WORD) {
+        state = OUTSIDE_THE_WORD;
+      }
       c = '\n';
-      putchar(c);
     }
+    else if (state == OUTSIDE_THE_WORD) {
+      state = INSIDE_THE_WORD;
+    }
+    putchar(c);
   }
 
   return (0);
