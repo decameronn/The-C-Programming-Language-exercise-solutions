2fbddd3f17861662fb9d5caca9c90fb4cf647c45
Constantin Zorile <czorile@gmail.com>
2021-03-11 23:02:57 +0200

    ex 1-13 more practice

--- chapter_1/exercise_1_13.c
+++ chapter_1/exercise_1_13.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
 
-#define HISTOGRAM_LEN 10
+#define MAX_WORD_HEIGHT 10
 #define MAX_WORD_LEN 10
 #define MIN_WORD_LEN 1
 
@@ -9,15 +9,38 @@ int main(void) {
   int c;
   int x, y;
   int word_len;  // counter for length of input words
-  int histogram[MAX_WORD_LEN][HISTOGRAM_LEN];
+  int histogram[MAX_WORD_LEN][MAX_WORD_HEIGHT];
 
   word_len = 0;
 
-  for (x = 0; x < MAX_WORD_LEN; ++x) {
-    for (y = 0; y < HISTOGRAM_LEN; ++y) {
-      histogram[x][y] = 0;
-      printf(" %d ", histogram[x][y]);
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\t' || c == '\n') {
+      /* don't do anything */
     }
-    printf("\n");
+    else if (word_len >= MIN_WORD_LEN && word_len <= MAX_WORD_LEN) {
+      if (word_len == 3) {
+        x = word_len;
+        ++y;
+      }
+    }
+
+    /* FIXME
+    if word_len == 1 then x == 1, y++ (=1)
+    if word_len == 2 then x == 2 (at position 2), y++ (increase y) = 1
+    if word_len == 2 then x == 2 (at position 2), y++ (increase y) = 2
+    if word_len == 2 then x == 2 (at position 2), y++ (increase y) = 3
+
+    word_len = ? (take c into account)
+    word_len = the actual x index (position on x axis)
+    */
+
+    for (x = 0; x < MAX_WORD_LEN; ++x) {
+      for (y = 0; y < MAX_WORD_HEIGHT; ++y) {
+        histogram[x][y] = 0;
+        printf(" %d ", histogram[x][y]);
+      }
+      printf("\n");
+    }
+
+    return (0);
   }
-}
