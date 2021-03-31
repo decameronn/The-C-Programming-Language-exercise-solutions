42780ebcebdc213396214ab1a4a7b4fb059cb0d9
Decameron <czorile@gmail.com>
2021-03-31 15:17:28 +0300

    remove incorrect code

--- chapter_1/exercie_1_14.c
+++ chapter_1/exercie_1_14.c
@@ -6,28 +6,7 @@
 
 int main(void) {
 
-  int i, j;
-  int arr[max];
-  int wc = 0;
-  int c;
-  
-  for (i = 0; i < max; ++i) arr[i] = 0;
-  
-  while((c = getchar()) != EOF) {
-    putchar(c);
-    ++wc;
-    
-    if (c == ' ' || c == '\t' || c == '\n') --wc;
-    if (wc >= min && wc <= max) ++arr[wc];
-  }
-
-  for (i = min; i <= max; ++i) {
-    printf("%3d > ", i);
-    for (j = 0; j < arr[i]; ++j) {
-        printf("*");
-    }
-    printf("\n");
-  }
+  /* code tbu */
 
   return (0);
 }
