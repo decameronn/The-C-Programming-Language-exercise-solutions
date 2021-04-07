e56328f8fbcacf6d03d315775b1b527c88b09abc
Constantin Zorile <czorile@gmail.com>
2021-04-07 21:15:14 +0300

    finish horizontal histogram

--- chapter_1/exercise_1_13.c
+++ chapter_1/exercise_1_13.c
@@ -8,11 +8,34 @@
 #define OUTSIDE_WORD 0
 
 int main(void) {
+
+  /**
+   * HORIZONTAL HISTOGRAM
+   * create an array, arr.length == len of the longest accepted word
+   * at each index, store word lengths corresponding to that index
+   * count word len: 
+   *  define a counter to store the value
+   *  as long as input is not a blank, counter++
+   * increase array index:
+   *  if we hit a blank, we increase the index at counter position
+   *  once we did that, we reset the counter and start counting again
+   * if the word len is larger than the defined value (10), normalize it
+   * at max value (or discard it)
+   * print:
+   *  first we print the array of lengths of words
+   *  after that, we iterate this array and print a given symbol (say, *)
+   *  that matches the number saved at that index (if we have 3 words
+   *  4 letters long each, we print arr[4] ***)
+   * 
+   *  VERTICAL HISTOGRAM
+   *  
+   */
+
   int c;
   int x, y;
   int state;
   int word_len, blank_len;
-  int histogram[MAX_WORD_LEN];
+  int histogram[MAX_WORD_LEN+1];
 
   word_len = 0;
   blank_len = 0;
