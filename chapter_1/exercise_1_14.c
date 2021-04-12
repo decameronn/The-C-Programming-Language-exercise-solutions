97787b894e525bc6551baadc30ff72ebc921b042
Constantin Zorile <czorile@gmail.com>
2021-04-12 22:33:11 +0300

    initial commit

--- chapter_1/exercise_1_14.c
+++ chapter_1/exercise_1_14.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-int main(void) {
+#define TOTAL_AMMOUNT 100
 
-    /* placeholder code */
-    
-    return (0);
+
+int main(void) {
+  /**
+   * Exercise 1-14. Write a program to print a histogram of the frequencies 
+   * of different characters in its input.
+   * 
+   * We assume only the ANSI input. If the input != ANSI, we ignore it.
+   */
+
+  int c;
+  int i, j;
+  int freq[36];
+
+  return (0);
 }
