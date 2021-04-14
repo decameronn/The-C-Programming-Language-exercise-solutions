fbdb0becae3d88682a59877e5e4b388e0aed9f30
Constantin Zorile <czorile@gmail.com>
2021-04-14 23:34:10 +0300

    simplify the syntax & logic

--- chapter_1/exercise_1_14.c
+++ chapter_1/exercise_1_14.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 
-#define MAX_FREQUNECY 100
 #define MAX_INPUT_TYPES 36
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE -1
 
 int main(void) {
   /**
    * Exercise 1-14. Write a program to print a histogram of the frequencies
    * of different characters in its input.
    *
-   * We assume only the ANSI input. If the input != ANSI, we ignore it.
+   * if input == some_input
+   *  ++arr[some_input] && print it
+   * if input != some_input
+   *  just ignore it && do not print it
    */
 
   int c;
   int i, j;
+  int chars, blanks, nums;
   int freq[MAX_INPUT_TYPES];
 
+  /** (0) initialize the data */
+  chars = blanks = nums = 0;
   for (i = 0; i < MAX_INPUT_TYPES; ++i) freq[i] = 0;
 
+  /** (1) determine the input type */
   while ((c = getchar()) != EOF) {
     putchar(c);
-    if ((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z')) {
-      // ++chars;
+    if (c >= '0' && c <= '9') {
+      ++freq[c - '0'];
     }
-    else if (c >= '0' && c <= '9') {
-      // ++nums;
+    else if (c == ' ' || c == '\t' || c == '\n') {
+      ++blanks;
     }
     else {
-      // ++blanks;
+      ++chars;
     }
   }
 
-  printf("%d\n", sizeof(unsigned long int));
-
-  return (0);
+  return EXIT_SUCCESS;
 }
