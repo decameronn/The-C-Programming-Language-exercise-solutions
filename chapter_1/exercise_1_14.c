a217f91f690c54d28b431805090b5aa573956acc
Constantin Zorile <czorile@gmail.com>
2021-04-14 22:20:03 +0300

    remove code related to wrong logic

--- chapter_1/exercise_1_14.c
+++ chapter_1/exercise_1_14.c
@@ -13,29 +13,20 @@ int main(void) {
 
   int c;
   int i, j;
-  unsigned int total;
-  unsigned int chars, nums, blanks;
-  float freq_chars, freq_nums, freq_blanks;
   int freq[MAX_INPUT_TYPES];
-
-  chars = nums = blanks = 0;
-  total = chars + nums + blanks;
-  freq_chars = chars/total;
-  freq_nums = nums/total;
-  freq_blanks = blanks/total;
 
   for (i = 0; i < MAX_INPUT_TYPES; ++i) freq[i] = 0;
 
   while ((c = getchar()) != EOF) {
     putchar(c);
     if ((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z')) {
-      ++chars;
+      // ++chars;
     }
     else if (c >= '0' && c <= '9') {
-      ++nums;
+      // ++nums;
     }
     else {
-      ++blanks;
+      // ++blanks;
     }
   }
 
