98fd3914c795f4f558793d2a6f10bb879b16adf8
Constantin Zorile <czorile@gmail.com>
2021-04-16 23:00:16 +0300

    temp commit

--- chapter_1/exercise_1_14.c
+++ chapter_1/exercise_1_14.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
+#define MIN_INPUT 1
 #define MAX_INPUT_TYPES 36
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE -1
+
+#ifndef log_error(x)
+#define log_error(x)                                     \
+  printf("Program encountered an error. Quitting...\n"); \
+  return EXIT_FAILURE
+#endif
 
 int main(void) {
   /**
    * Exercise 1-14. Write a program to print a histogram of the frequencies
    * of different characters in its input.
-   *
-   * if input == some_input
-   *  ++arr[some_input] && print it
-   * if input != some_input
-   *  just ignore it && do not print it
    */
 
   int c;
   int i, j;
   int chars, blanks, nums;
+  int total;
   int freq[MAX_INPUT_TYPES];
 
   /** (0) initialize the data */
-  chars = blanks = nums = 0;
+  chars = blanks = nums = total = 0;
   for (i = 0; i < MAX_INPUT_TYPES; ++i) freq[i] = 0;
 
   /** (1) determine the input type */
@@ -37,6 +40,15 @@ int main(void) {
       ++chars;
     }
   }
+  
+  /** horizontal histogram */
+  for (i = MIN_INPUT; i <= MAX_INPUT_TYPES; ++i) {
+    if (c != ' ' || c != '\t' || c != '\n') {
+      printf("%d", i);
+    }
+    printf("\n");
+  }
+
 
   return EXIT_SUCCESS;
 }
