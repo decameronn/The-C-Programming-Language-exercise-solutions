f27cfedd86920f51eb81662c34e5e67f9f8b6aea
Constantin Zorile <czorile@gmail.com>
2021-04-18 21:53:10 +0300

    use relative paths instead of absolute paths

--- chapter_1/_compile.bat
+++ chapter_1/_compile.bat
@@ -1,5 +1,6 @@
 :: @brief Find the newest .c file in a folder and compile it with gcc
 :: @author Decameron
+:: my_path = the path to gcc executable
 
 @echo off
 for /f "delims=" %%f in ('dir /b /a-d /od /tw %cd%\*.c') do (
@@ -8,5 +9,5 @@ for /f "delims=" %%f in ('dir /b /a-d /od /tw %cd%\*.c') do (
 
 echo The newest file is: %file%
 echo Compiling %file% with GCC...
-call C:\Applications\mingw-w64\mingw64\bin\gcc.exe ^
+call %my_path%\gcc.exe ^
   -Wall -Wmissing-prototypes -Wextra -O2 -std=c90 -ggdb3 %file%
