00f957ab76949cb65e573e7c381d4523aeef816b
Constantin Zorile <czorile@gmail.com>
2021-04-18 20:51:52 +0300

    add compilation option via bat file

---
+++ chapter_1/_compile.bat
@@ -0,0 +1,12 @@
+:: @brief Find the newest .c file in a folder and compile it with gcc
+:: @author Decameron
+
+@echo off
+for /f "delims=" %%f in ('dir /b /a-d /od /tw %cd%\*.c') do (
+  set file=%%f
+)
+
+echo The newest file is: %file%
+echo Compiling %file% with GCC...
+call C:\Applications\mingw-w64\mingw64\bin\gcc.exe ^
+  -Wall -Wmissing-prototypes -Wextra -O2 -std=c90 -ggdb3 %file%
