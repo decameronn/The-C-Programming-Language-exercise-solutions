91bbf80cba897b539cb16d4e13406984dd9849b5
Constantin Zorile <czorile@gmail.com>
2021-04-21 23:49:44 +0300

    exercise 1-15

--- chapter_1/exercise_1_15.c
+++ chapter_1/exercise_1_15.c
@@ -1,26 +1,31 @@
-/** this is not exercise 1-15.
- * this is the example from chapter 1.7 */
-
 #include <stdio.h>
 
-int power(int, int);
+#define MAX 300
+#define MIN 0
+#define STEP 20
+
+void fahr_to_celsius(void);
 
 int main(void) {
-  int i;
-  for (i = 0; i < 10; ++i) {
-    printf("%d %d %d\n", i, power(2, i), power(-3, i));
-  }
+  /* Exercise 1-15. Rewrite the temperature conversion program of Section 1.2 to
+  use a function for conversion. */
+
+  fahr_to_celsius();
 
   return 0;
 }
 
-/** n >= 0 */
-int power(int base, int n) {
-  int i, p;
-  p = 1;
+void fahr_to_celsius(void) {
+  float fahr, celsius;
 
-  for (i = 1; i <= n; ++i) {
-    p = p * base;
+  printf("**********************\n");
+  printf(" Fahrenheit | Celsius\n");
+  printf("**********************\n");
+
+  fahr = MIN;
+  while (fahr <= MAX) {
+    celsius = (5.0f / 9.0f) * (fahr - 32.0f);
+    printf("%6.0f %13.1f\n", fahr, celsius);
+    fahr += STEP;
   }
-  return p;
 }
