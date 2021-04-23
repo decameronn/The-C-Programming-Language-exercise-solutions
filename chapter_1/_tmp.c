fc4a3455c1d6c1b4cbd99ec99e237cdba2dd423f
Constantin Zorile <czorile@gmail.com>
2021-04-23 22:44:56 +0300

    awesome commit

--- chapter_1/_tmp.c
+++ chapter_1/_tmp.c
@@ -19,14 +19,26 @@ void copy(char to[], char from[]);
 
 int main(void) {
 
-  /* some code */
+  int len, max;
+  char line[MAXLINE];
+  char longest[MAXLINE];
+
+  max = 0;
+  while ((len = getline(line, MAXLINE)) > 0) {
+    if (len > max) {
+      max = len;
+      copy(longest, line);
+    }
+    if (max > 0) printf("%s", longest);
+  }
 
   return 0;
 }
 
-int getline(char line[], int maxline) {
+/* read a line into line[], return the length */
+int getline(char line[], int len) {
   int c, i;
-  for (i = 0; i < maxline - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+  for (i = 0; i < len - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
     line[i] = c;
   }
   if (c == '\n') {
@@ -35,4 +47,12 @@ int getline(char line[], int maxline) {
   }
   line[i] = '\0';
   return i;
+}
+
+/* copy "from" into "to"; assume "to" is big enough */
+void copy(char to[], char from[]) {
+  int i;
+  
+  i = 0;
+  while((to[i] = from[i]) != '\0') ++i;
 }
