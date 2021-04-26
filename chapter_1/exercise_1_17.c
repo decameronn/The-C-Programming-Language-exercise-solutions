63ac4609ab44c729d76872509e00dc4909e78ad0
Constantin Zorile <czorile@gmail.com>
2021-04-26 23:21:49 +0300

    small iteration for 1-17

--- chapter_1/exercise_1_17.c
+++ chapter_1/exercise_1_17.c
@@ -1,38 +1,51 @@
 /**
- * Exercise 1-17. Write a program to print all input lines that are longer 
+ * Exercise 1-17. Write a program to print all input lines that are longer
  * than 80 characters.
  */
 
 #include <stdio.h>
 
-#define MAXLINE       1000
-#define EXIT_SUCCESS  0
-#define EXIT_FAILURE  1
+#define MAXLEN 80
+#define MINLEN 1
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
 
 int getline(char s[], int len);
 void copy(char to[], char from[]);
 
 int main(void) {
+  /**
+   * while (there's a line)
+   *  if (current line's len > 80)
+   *    save current line
+   *    print current line
+   */
 
-/**
- * while (there's a line)
- *  if (current line's len > 80)
- *    save current line
- *    print current line
- */
+  int len = 0;
+  volatile int short_len = 0;
+  char line[MAXLEN] = {0};
 
-  int len;
-
-  char line[MAXLINE] = {0};
-  len = getline(line, MAXLINE);
-
-  while (len > 80) {
-    printf("%s\n", line);
-
-    if (len <= 80) {
-      printf("Line shorter than 80 characters. Exiting...\n");
-      return EXIT_FAILURE;
+  while ((len = getline(line, MAXLEN)) > 0) {
+    ++len;
+    
+    if (len > MAXLEN) {
+      printf("%s\n", line);
     }
+    // else if (len >= MINLEN && len <= MAXLEN) {
+    //   short_len = len;
+    //   printf("Line shorter than 80 characters.\n");
+    // } 
+
+    // if (len >= MINLEN && len <= MAXLEN) {
+    //   printf("Line shorter than 80 characters. Exiting...\n");
+    //   return EXIT_FAILURE;
+    // } 
+    // else if (len > MAXLEN) {
+    //   printf("%s\n", line);
+    // } 
+    // else {
+    //   return EXIT_FAILURE;
+    // }
   }
 
   return EXIT_SUCCESS;
