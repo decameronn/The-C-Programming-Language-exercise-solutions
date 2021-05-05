06ede474ad066f8495c1573bdb7e043787abc74c
Constantin Zorile <czorile@gmail.com>
2021-05-05 22:39:06 +0300

    exercise 1-17

--- chapter_1/exercise_1_17.c
+++ chapter_1/exercise_1_17.c
@@ -5,35 +5,25 @@
 
 #include <stdio.h>
 
-#define MAXLEN 1000
+#define MAXLEN 85
 #define CURLEN 80
 
 int getline(char s[], int len);
-void copy(char to[], char from[]);
 
 int main(void) {
-  /**
-   * while (there's a line)
-   *  if (current line's len > 80)
-   *    save current line
-   *    print current line
-   *  print the number of lines with len > 80 (optional)
-   */
 
   int len = 0;
   int num_of_lines = 0;
   char line[MAXLEN] = {0};
-  char current_line[MAXLEN] = {0};
 
   while ((len = getline(line, MAXLEN)) > 0) {
-    if (len > CURLEN && len <= MAXLEN) {
-      ++num_of_lines;
-      copy(current_line, line);
-      printf("%s", current_line);
-    } 
-    else if (len > MAXLEN) {
+    if (len > MAXLEN) {
       printf("Line is longer than 1000 characters. Aborting...\n");
       return -1;
+    } 
+    else if (len > CURLEN && len <= MAXLEN) {
+      ++num_of_lines;
+      printf("%s", line);
     } 
     else { /* do nothing */ }
   }
@@ -54,12 +44,4 @@ int getline(char s[], int len) {
   }
   s[i] = '\0';
   return i;
-}
-
-/* copy "from" into "to"; assume "to" is big enough */
-void copy(char to[], char from[]) {
-  int i = 0;
-  while ((to[i] = from[i]) != '\0') {
-    ++i;
-  }
 }
